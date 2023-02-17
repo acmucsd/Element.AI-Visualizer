c6b06df586dd6958b25916f2ba08b276be18169e
Jonah <volcanobud@gmail.com>
2023-02-17 00:57:01 -0800

    video outputs and textures

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
 
-# pygame.surfarray as surfarray
-from pygame.examples.arraydemo import surfdemo_show
-import sys
 import json
 import numpy as np
-
+import cv2
 from visualizer import Visualizer
+import argparse
 
-f = open('test.json')
+#argument parser, default is render through pygame and no output video file
+parser = argparse.ArgumentParser(description="Run the Element.AI Visualizer game.")
+parser.add_argument("-r", "--render", action='store_true', help="turn on pygame rendering")
+parser.add_argument("-o", "--output", help="Where to output replays. Default is none and no replay is generated")
+parser.add_argument("-f", "--file", help="Where to read files. Default is test.json", default="test.json")
+args = parser.parse_args()
+
+#open input json file
+f = open(args.file)
 data = json.load(f)
 
+#load information about board like size
 first_obs = np.array(data['observations'][0]['board']['board_state'])
 map_size = first_obs.shape[0]
+
+#initialize visualizer
 py_visualizer = Visualizer(map_size)
 py_visualizer.init_window()
-for obs in data['observations']:
+w,h = py_visualizer.dims()
 
+#initialize video writer
+if args.output != None:
+    out = cv2.VideoWriter(args.output, cv2.VideoWriter_fourcc(*'mp4v'), 5, (int(w),int(h)))
+
+i = 0
+#iterate through all observations
+for obs in data['observations']:
     grid = np.array(obs['board']['board_state'])
     player_num_grid = np.array(obs['board']['players_state'])
     num_agents = len(obs.keys()) - 1
@@ -24,6 +40,20 @@ for obs in data['observations']:
     for key in obs.keys():
         if key != 'board':
             heads.append(obs[key]['head'])
+    #update visual scene
+    rgb = py_visualizer.update_scene(grid, player_num_grid, num_agents, heads)
+    #print iteration number
+    i += 1
+    print(i)
+    #if render, update screen
+    if args.render == False:
+        py_visualizer.render()
+    #if output file, write to output video
+    if args.output != None:
+        rgb = rgb.swapaxes(0, 1)
+        out.write(cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
+#release output video
+if args.output != None:
+    out.release()
 
-    py_visualizer.update_scene(grid, player_num_grid, num_agents, heads)
-    py_visualizer.render()
+
--- visualizer.py
+++ visualizer.py
@@ -1,7 +1,7 @@
 import pygame
 import numpy as np
 from time import sleep
-
+import cv2
 import math
 TEMP, UNOCCUPIED, PASSED, OCCUPIED, BOMB, BOOST = -1, 0, 1, 2, 3, 4
 class Visualizer:
@@ -12,104 +12,165 @@ class Visualizer:
 
         self.angle_factor = 2.4
         self.inc = 100
+
         self.block_width = 16#math.ceil(200*69.19/86.52);
         self.block_height = self.block_width*ground_h/ground_w
+        self.island_height = math.ceil(size * 1/5)
         self.block_adj = self.block_height#(self.block_height - 19.08/69.15*self.block_height)-1/200*self.block_height#*(ground_h/ground_w)*
 
 
-        self.player_width = 8
-        self.player_height = 8
+        self.player_width = self.block_width
+        self.player_height = self.player_width
         self.map_size = size
-        self.WINDOW_HEIGHT = math.ceil(self.block_height*self.map_size) + self.inc#math.ceil(self.block_adj * self.map_size + self.block_height - self.block_adj) + self.inc
+        self.WINDOW_HEIGHT = math.ceil(self.block_height*self.map_size) + self.inc + self.island_height*self.block_height/2#math.ceil(self.block_adj * self.map_size + self.block_height - self.block_adj) + self.inc
         self.WINDOW_WIDTH = self.block_width * self.map_size + self.inc
 
-        self.ground_img = pygame.transform.scale(pygame.image.load('ground5.png'), (self.block_width, self.block_height))
+        self.bomb_img = pygame.transform.smoothscale(pygame.image.load('bomb2.png'), (self.player_width, self.player_height))
+        self.boost_img = pygame.transform.smoothscale(pygame.image.load('boost.png'), (self.player_width, self.player_height))
+        self.explosion = pygame.transform.smoothscale(pygame.image.load('explosion.png'), (self.player_width, self.player_height))
+
+        self.ground_img = pygame.transform.smoothscale(pygame.image.load('bigGround.png'), (self.block_width, self.block_height))
+        self.ground_left = pygame.transform.smoothscale(pygame.image.load('16x10 left big.png'), (self.block_width/2, 13/8*self.block_width/2))
+        self.ground_right = pygame.transform.smoothscale(pygame.image.load('16x10 rightdark.png'), (self.block_width/2, 13/8*self.block_width/2))
+
         self.fire_ground = pygame.transform.scale(pygame.image.load('fireGround2.png'), (self.block_width, self.block_height))
         self.water_ground = pygame.transform.scale(pygame.image.load('waterGround.png'), (self.block_width, self.block_height))
+        self.earth_ground = pygame.transform.scale(pygame.image.load('earth travel.png'),
+                                                  (self.block_width, self.block_height))
+        self.air_ground = pygame.transform.scale(pygame.image.load('air travel.png'),
+                                                   (self.block_width, self.block_height))
 
-        self.p1_img = pygame.transform.scale(pygame.image.load('fireElementEdited.png'), (self.player_width, self.player_height))
-        #self.boost_array = pygame.surfarray.array3d(boost_img)
-        self.eps = 0
+        self.fire_ter = pygame.transform.smoothscale(pygame.image.load('lava ground.png'), (self.block_width, self.block_height))
+        self.water_ter = pygame.transform.smoothscale(pygame.image.load('water ground.png'),
+                                                     (self.block_width, self.block_height))
+        self.earth_ter = pygame.transform.smoothscale(pygame.image.load('earth ground.png'),
+                                                      (self.block_width, self.block_height))
+        self.air_ter = pygame.transform.smoothscale(pygame.image.load('air ground.png'),
+                                                      (self.block_width, self.block_height))
 
+
+
+        self.fire_img = pygame.transform.smoothscale(pygame.image.load('fireElementEdited.png'), (self.player_width, self.player_height))
+        self.water_img = pygame.transform.smoothscale(pygame.image.load('water element.png'),
+                                               (self.player_width, self.player_height))
+        self.earth_img = pygame.transform.smoothscale(pygame.image.load('earth element.png'),
+                                                      (self.player_width, self.player_height))
+        self.air_img = pygame.transform.smoothscale(pygame.image.load('air element.png'),
+                                                      (self.player_width, self.player_height))
+        self.old_head = [(-1,-1), (-1,-1), (-1,-1), (-1,-1)]
 
     def init_window(self):
 
         pygame.init()
         pygame.display.init()
         self.screen = pygame.display.set_mode((self.WINDOW_WIDTH, self. WINDOW_HEIGHT))
-        self.screen.fill((255, 255, 255))
-        self.draw_grid()
+        self.screen.fill((173,216,230))
+        #self.draw_grid()
         pygame.display.update()
 
+    #helper draw initialize grid
     def draw_grid(self):
-        # for x in range(0, self.WINDOW_WIDTH, self.block_width):
-        #     for y in range(0, self.WINDOW_HEIGHT, self.block_height):
-        #         rect = pygame.Rect(x, y, self.block_width, self.block_height)
-        #         pygame.draw.rect(self.screen, color.BLACK, rect, 1)
-        #self.block_width = self.ground_img.get_rect().x
-        #self.block_height = self.ground_img.get_rect().y
-
         for r in range(self.map_size):
             for c in range(self.map_size):
-                if(r > -1 and c > -1):
-                    x = (r-c-1)*self.block_width / 2 + self.WINDOW_WIDTH/2
-                    y = (r+c)*self.block_height/2 + self.inc/2
-                    self.screen.blit(self.ground_img, (x, y))
+                x = (r-c-1)*self.block_width / 2 + self.WINDOW_WIDTH/2
+                y = (r+c)*self.block_height/2 + self.inc/2
+                self.screen.blit(self.ground_img, (x, y))
+                if c == self.map_size - 1:
+                    if(r < self.island_height):
+                        z = r+1
+                    else:
+                        z = self.island_height
+                    for i in range(z):
+                        fY = (r+c+i)*self.block_height/2 + self.inc/2
+                        self.screen.blit(self.ground_left, (x, fY+self.block_height/2))
+                if r == self.map_size - 1:
+                    if (c < self.island_height):
+                        z = c+1
+                    else:
+                        z = self.island_height
+                    for i in range(z):
+                        fY = (r + c + i) * self.block_height / 2 + self.inc / 2
+                        self.screen.blit(self.ground_right, (x+self.block_width/2, fY + self.block_height / 2))
                     #print(str(r) + ": "  + str(x) + " " + str(c) + ": " + str(y) + "\n")
 
     def render(self):
         pygame.event.pump()
         pygame.display.update()
-        sleep(0.5)
-
+        sleep(0.1)
+    def rgbRender(self, rgb):
+        self.screen.blit(pygame.surfarray.make_surface(rgb), (0,0))
+        pygame.display.update()
+        pygame.event.pump()
+    def dims(self):
+        return [self.WINDOW_WIDTH,self.WINDOW_HEIGHT]
     def update_scene(self, grid, player_num_grid, num_agents, heads):
+        self.screen.fill((173,216,230)) # clear canvas
 
-        self.player_imgs = [(self.p1_img, self.fire_ground),
-                            (self.p1_img, self.water_ground)
+        self.player_imgs = [(self.fire_img, self.fire_ground, self.fire_ter),
+                            (self.water_img, self.water_ground, self.water_ter),
+                            (self.earth_img, self.earth_ground, self.earth_ter),
+                            (self.air_img, self.air_ground, self.air_ter)
                             ]
 
         for r in range(self.map_size):
             for c in range(self.map_size):
                 x = self.WINDOW_WIDTH / 2 - self.block_width / 2 + (r - c) * (self.block_width) / 2
                 y = self.inc / 2 + (r + c) * (self.block_height) / 2
-                #spriteX = x + (self.block_width - self.player_width) / 2
-                #spriteY = y - (self.block_height - self.player_height) / 2
+
                 if(x >= self.WINDOW_WIDTH or y >= self.WINDOW_HEIGHT):
                     raise Exception("Out of Bounds Exception")
                 if grid[r][c] == PASSED:
                     self.screen.blit(self.player_imgs[player_num_grid[r][c]][1], (x, y))
                 elif grid[r][c] == OCCUPIED:
-                    self.screen.blit(self.player_imgs[player_num_grid[r][c]][1], (x, y))
+                    self.screen.blit(self.player_imgs[player_num_grid[r][c]][2], (x, y))
                 elif grid[r][c] == UNOCCUPIED:
                     self.screen.blit(self.ground_img, (x, y))
+                #redraw island boundaries
+                if c == self.map_size - 1:
+                    if(r < self.island_height):
+                        z = r+1
+                    else:
+                        z = self.island_height
+                    for i in range(z):
+                        fY = (r+c+i)*self.block_height/2 + self.inc/2
+                        self.screen.blit(self.ground_left, (x, fY+self.block_height/2))
+                if r == self.map_size - 1:
+                    if (c < self.island_height):
+                        z = c+1
+                    else:
+                        z = self.island_height
+                    for i in range(z):
+                        fY = (r + c + i) * self.block_height / 2 + self.inc / 2
+                        self.screen.blit(self.ground_right, (x+self.block_width/2, fY + self.block_height / 2))
+
+        for r in range(self.map_size):
+            for c in range(self.map_size):
+                x = self.WINDOW_WIDTH / 2 - self.block_width / 2 + (r - c) * (self.block_width) / 2
+                y = self.inc / 2 + (r + c) * (self.block_height) / 2
+                spriteX = x + 2#+ (self.block_width - self.player_width) / 2
+                spriteY = y + (self.block_height - self.player_height) / 2 - 3
+                if(x >= self.WINDOW_WIDTH or y >= self.WINDOW_HEIGHT):
+                    raise Exception("Out of Bounds Exception")
+                if grid[r][c] == BOMB:
+                    self.screen.blit(self.ground_img, (x, y))
+                    self.screen.blit(self.bomb_img, (spriteX, spriteY-1))
+                elif grid[r][c] == BOOST:
+                    self.screen.blit(self.ground_img, (x, y))
+                    self.screen.blit(self.boost_img, (spriteX, spriteY-2))
+
+
+        #small bug, sometimes player doesn't correspond to same index?
+        i = 0
         for head in heads:
             hX, hY = head
-            x = self.WINDOW_WIDTH / 2 - self.block_width / 2 + (hY - hX) * (self.block_width) / 2 + + (self.block_width - self.player_width) / 2
-            y = self.inc / 2 + (hY + hX) * (self.block_height) / 2 - (self.block_height - self.player_height) / 2
-            self.screen.blit(self.player_imgs[0][0], (x,y))
+            if(hX == -1 and hY == -1):
+                self.screen.blit(self.explosion, (self.old_head[i][0], self.old_head[i][1]))
+                continue
+            x = self.WINDOW_WIDTH / 2 - self.block_width / 2 + (hY - hX) * (self.block_width) / 2 #+ (self.block_width - self.player_width) / 2
+            y = self.inc / 2 + (hY + hX) * (self.block_height) / 2 + (self.block_height - self.player_height) / 2 - 3
+            self.screen.blit(self.player_imgs[i][0], (x,y))
+            self.old_head[i] = (x,y)
+            i += 1
+        #for video
+        return pygame.surfarray.array3d(self.screen)
 
-                    #print("hi why i no draw :(")
-                    #self.screen.blit(self.p1_img, ((r-c)*self.block_width / 2 + (r-c)*(self.block_width-self.player_width)/2 + self.WINDOW_WIDTH/2, 30 + (r+c)*self.block_adj/2))
-                #pygame.draw.rect(self.screen, color.BLACK, rect, 1)
-                #self.screen.blit(self.bomb_img, (x,y))
-                #self.screen.blit(self.ground_img, (x/2, y/2))
-                # if grid[r][c] == UNOCCUPIED:
-                #     #self.rgb_array[r][c] = WHITE_SMOKE
-                #     #pygame.draw.rect(self.screen, color.WHITE, rect, 0)
-                #     self.screen.blit(self.ground_img, (x,y))
-                # elif grid[r][c] == BOMB:
-                #     #self.rgb_array[r][c] = BLACK
-                #     self.screen.blit(self.bomb_img, (x,y))
-                # elif grid[r][c] == BOOST:
-                #     #self.rgb_array[r][c] = PURPLE
-                #     self.screen.blit(self.boost_img, (x,y))
-                # elif grid[r][c] == PASSED:
-                #     #self.rgb_array[r][c] = self.player_colors[player_num_grid[r][c]][0]
-                #     #pygame.draw.rect(self.screen, self.player_colors[player_num_grid[r][c]][0], rect, 0)
-                #     # pygame.draw.rect(self.screen, color.BLACK, rect, 1)
-                #     self.screen.blit(self.player_imgs[player_num_grid[r][c]][0], (x,y))
-                # elif grid[r][c] == OCCUPIED:
-                #     #self.rgb_array[r][c] = self.player_colors[player_num_grid[r][c]][1]
-                #     self.screen.blit(self.player_imgs[player_num_grid[r][c]][1], (x,y))
-                # else:
-                #     raise Exception("Unknown grid value")
